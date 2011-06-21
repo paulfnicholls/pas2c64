89aa82766364312ca5d2cd1360adc600b5d1e5de
paulfnicholls@gmail.com <paulfnicholls@gmail.com@773bb0cd-a19a-fed7-1242-d46b00016b8f>
2011-06-21 06:37:08 +0000

    Now supports 6502 assembler procedures!!

--- trunk/Documentation/General c64 documentation/PETSCII To Screen Code.asm
+++ trunk/Documentation/General c64 documentation/PETSCII To Screen Code.asm
@@ -3,7 +3,7 @@
 ;CONVERT CBM PETSCII to SCREEN CODE 
 ; 
 ;   ---------------------- 
-;   .A = PETSCII character 
+;   .A = PETSCII character
 ;   ---------------------- 
 ;   .A = screen code 
 ;   .X = entry value 
@@ -60,3 +60,22 @@ petcod02 cmp #petcodch+1       ;high control code end
 ; 
 petcod03 rts 
 ;
+
+Commodore 64 PETSCII code to screen code conversion
+
+PETSCII code	   Change     Screen code
+-------------------------------------------------------------
+0-31	 $00-$1F | +128	$80 | 128-159	$80-$9F
+32-63	 $20-$3F |  0	$00 | 32-63	$20-$3F
+64-95	 $40-$5F | -64	$C0 | 0-31	$00-$1F
+96-127	 $60-$7F | -32	$E0 | 64-95	$40-$5F
+128-159	 $80-$9F | +64	$40 | 192-223	$C0-$DF
+160-191	 $A0-$BF | -64	$C0 | 96-127	$60-$7F
+192-223	 $C0-$DF | -128	$80 | 64-95	$40-$5F
+224-254	 $E0-$FE | -128	$80 | 96-126	$60-$7E
+255	 $FF	 |  94	$5E
+
+Notes:
+
+PETSCII code $FF is the BASIC token of the p (pi) symbol.
+It is converted internally to screen code $5E when printed onto the screen.
